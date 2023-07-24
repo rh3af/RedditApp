cee5df21fd9d4f399394dc8141ae5fa51ae03438
Rh3af <ram.hemanth3@gmail.com>
2023-07-24 14:56:29 -0300

    Made Project

---
+++ src/test/java/com/reddit/RedditApp/RedditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddit.RedditApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
